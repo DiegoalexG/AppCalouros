69f95385e26a775217f6b1572cbdde2a2a25759e
DiegoA <diego.alecsander_agui@hotmail.com>
2019-02-12 17:48:05 -0200

    mapa

--- App/src/components/Mapa.js
+++ App/src/components/Mapa.js
@@ -1,12 +1,127 @@
 import React from 'react';
-import { StyleSheet, View, Text, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, View, ImageBackground, Dimensions, ScrollView } from 'react-native';
 import { Header, Left, Right, Icon, Button } from 'native-base';
+import Map from './Map';
+import ContatosCollapsiblePanel from './ContatosCollapsiblePanel';
 
 const banner = require('../../assets/imgs/bannerSombreado.png');
 
 export default class Mapa extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'map',
+      my_position: false,
+      places: [
+        {
+          latitude: 1,
+          longitude: 1,
+          nome: 'teste',
+          img: '../../assets/imgs/bannerSombreado.png',
+          descricao: 'testetbm'
+        },
+        {
+          latitude: 2,
+          longitude: 3,
+          nome: 'teste',
+          img: '../../assets/imgs/bannerSombreado.png',
+          descricao: 'testetbm'
+        },
+        {
+          latitude: 3,
+          longitude: 3,
+          nome: 'teste',
+          img: '../../assets/imgs/bannerSombreado.png',
+          descricao: 'testetbm'
+        },
+        {
+          latitude: 4,
+          longitude: 4,
+          nome: 'teste',
+          img: '../../assets/imgs/bannerSombreado.png',
+          descricao: 'testetbm'
+        },
+        {
+          latitude: 5,
+          longitude: 5,
+          nome: 'teste',
+          img: '../../assets/imgs/bannerSombreado.png',
+          descricao: 'testetbm'
+        },
+      ]
+    };
+  }
+
+  _changeMode = () => {
+    if (this.state.mode == 'map') {
+      this.setState({ mode: 'list-ul' });
+    } else {
+      this.setState({ mode: 'map' });
+    }
+  }
+
+  _mapReady = async (mapView) => {
+    await this._getPosition();
+
+    mapView.animateToCoordinate(this.state.my_position, 0);
+  };
+
+  _getPosition = () => {
+    return new Promise((resolve, reject) => {
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          let my_position = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+
+          this.setState({ my_position });
+
+          resolve(my_position);
+
+        },
+        (error) => {
+          this._getPosition();
+        },
+        { timeout: 10000, maximumAge: 0 }
+        // { enableHighAccuracy: true, timeout: 10000, maximumAge: 0}
+      );
+      
+    });
+  }
+
+  renderList = () => {
+    let list = [];
+    this.state.places.map( (place, i) => {
+      list.push(
+        <ContatosCollapsiblePanel 
+          key={i}
+          titulo={place.nome}
+          img={place.img}
+          descricao={place.descricao}
+        />
+      );
+    });
+    return list;
+  }
+
   render() {
+    const icon = (this.state.mode === 'map') ?
+      <Icon
+        name="menu"
+        color="black"
+        size={28}
+        onPress={() => this._changeMode()}
+      />
+      :
+      <Icon
+        name="menu"
+        color="black"
+        size={28}
+        onPress={() => this._changeMode()}
+      />;
     return (
       <View style={styles.container} >
         <ImageBackground
@@ -20,10 +135,28 @@ export default class Mapa extends React.Component {
                 <Icon name='menu' onPress={() => this.props.navigation.openDrawer()} />
               </Button>
             </Left>
-            <Right />
+            <Right>
+              <Button transparent >
+                {icon}
+              </Button>
+            </Right>
           </Header>
         </ImageBackground>
-        <Text>teste</Text>
+        {(this.state.mode === 'map') ?
+          <Map
+            
+            rotate={false}
+            places={this.state.places}
+            onMapReady={this._mapReady}
+            my_position={this.state.my_position}
+            navigation={this.props.navigation}
+            view="MapsScreen"
+          />
+          :
+          <ScrollView style={{ marginTop: 70 }}>
+            {this.renderList()}
+          </ScrollView>
+        }
       </View>
     );
   }
---
+++ App/src/components/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+
+const { width } = Dimensions.get('window');
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    mapView: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+
+    placesContainer: {
+        position: "absolute",
+        bottom: 0,
+        width: '100%',
+    },
+
+    place: {
+        width: width - 40,
+        height: 150,
+        backgroundColor: 'rgba(25,25,112,.8)',
+        marginHorizontal: 20,
+        borderRadius: 10,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        padding: 20,
+        paddingBottom: 5,
+    },
+});
